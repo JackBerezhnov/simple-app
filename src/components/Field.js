54ff70de9d7b8ba8f46f0a2418d7af06ae5e40f8
Evgeny Berezhnov <jackberezhnov@gmail.com>
2019-09-29 20:13:41 -0700

    start easy app

---
+++ src/components/Field.js
@@ -0,0 +1,15 @@
+import './Field.css';
+import React, { Component } from 'react';
+
+
+class Field extends Component {
+    render() {
+        return(
+            <div className="field">
+
+            </div>
+        )
+    }
+}
+
+export default Field;
