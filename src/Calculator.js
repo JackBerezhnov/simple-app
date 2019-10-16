1c10ccf425198195f5ea149eebb70cc73c9019dc
Evgeny Berezhnov <jackberezhnov@gmail.com>
2019-10-16 12:05:18 -0700

    added one more input

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import './App.css';
+import TemperatureInput from './components/TemperatureInput';
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {temperature: ''};
-  }
-
-  handleChange(e) {
-    this.setState({temperature: e.target.value});
-  }
 
   render() {
-    const temperature = this.state.temperature;
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input 
-          value={temperature}
-          onChange={this.handleChange} />
-
-        <BoilingVerdict 
-          celsius={parseFloat(temperature)} />
-      </fieldset>
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
     );
   }
 }
